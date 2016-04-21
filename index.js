23cc5adb6b22da20f4333c6752e97cb5d12df31b
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-21 16:09:50 -0700

    Playing with functions

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ var fs = require('fs'),
 
 filenames = fs.readdirSync(".");
 
-for(i = 0; i < filenames.length; i++) {
+for (i = 0; i < filenames.length; i++) {
     console.log(filenames[i]);
 }
 console.log("Ready.");
+console.log('after the iteration blocking\n\n\n');
 
 // Doesn't work in Windows allegedly
 // processId = process.getuid();
+
+function iterateThroughFiles(filenames) {
+    for (i = 0; i < filenames.length; i++) {
+        console.log(filenames[i]);
+    }
+}
+
+fs.readdir('.', function (err, filenames) {
+    filenames.forEach(function (item) {
+        console.log(item);
+    })
+    console.log("Ready.");
+});
+
+console.log('after the iteration async');
