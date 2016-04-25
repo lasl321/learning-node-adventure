fb27e3f2ee645f2dd56f46b412278ea75c147941
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-25 13:15:10 -0700

    Added code to get number of cores

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-var fs = require('fs'),
-    filenames,
-    i,
-    processId;
+const numCPUs = require('os').cpus().length;
 
-filenames = fs.readdirSync(".");
+console.log(numCPUs);
 
-for (i = 0; i < filenames.length; i++) {
-    console.log(filenames[i]);
-}
-console.log("Ready.");
-console.log('after the iteration blocking\n\n\n');
-
-// Doesn't work in Windows allegedly
-// processId = process.getuid();
-
-function iterateThroughFiles(filenames) {
-    for (i = 0; i < filenames.length; i++) {
-        console.log(filenames[i]);
-    }
-}
-
-fs.readdir('.', function (err, filenames) {
-    filenames.forEach(function (item) {
-        console.log(item);
-    })
-    console.log("Ready.");
-});
-
-console.log('after the iteration async');
+console.log(`Danger ${numCPUs}`);
