b00c1e02c7f0d3bf82099f14aadd8031966f4a09
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-27 15:07:30 -0700

    Playing with uncaught exceptions

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var express = require('express');
-var app = express();
+var fs = require('fs');
+function readJSONThrows(filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      return callback(err);
+    }
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+    callback(null, JSON.parse(data));
+  })
+}
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// Try-catch blocks will not work since the callback is in a different stack
+try {
+  readJSONThrows('printer.js', function (err) {
+    console.log(err);
+  });
+} catch (e) {
+  console.log(e);
+}
+
+process.on('uncaughtException', err => {
+  console.error('Parse error', err);
+  /**
+   * It is advised, especially in production, to exit anyway from the
+   * application after an uncaught exception is received.
+   */
+  process.exit(1);
 });
