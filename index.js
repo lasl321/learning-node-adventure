1bf1dae18b0cf58f5c24725ad8eac9df85f00dbe
Luis Sotomayor <LSotomayor@futuredontics.com>
2016-04-27 16:57:58 -0700

    Playing with event emitter

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-var f = require('./testclass');
-console.log(new f.TestClass().doSomething());
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+
+function readFile(files) {
+  var emitter = new EventEmitter();
+  files.forEach(function (file) {
+    fs.readFile(file, function (err, content) {
+      if (err) {
+        /**
+         * The EventEmitter cannot just throw exceptions when an error condition
+         * occurs, as they would be lost in the event loop if the event is emitted
+         * asynchronously. Instead, the convention is to emit a special event, called
+         * error, and to pass an Error object as an argument.
+         */
+        return emitter.emit('error', err, file);
+      }
+
+      emitter.emit('fileread', file, content);
+    });
+  });
+
+  return emitter;
+}
+
+var e = readFile(['index.js', 'foo.js']);
+
+e.on('fileread', function (file, content) {
+  console.log('file read', file, content);
+});
+
+e.on('error', function (err, file) {
+  console.log('error', file, err);
+});
