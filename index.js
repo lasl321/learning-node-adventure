1050c71ccb0783392516999e36fe27d1c1d0e4ff
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-04-27 20:59:51 -0700

    Playing with custom emitter

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
+'use strict';
+
 var EventEmitter = require('events').EventEmitter;
 var fs = require('fs');
+var util = require('util');
 
-function readFile(files) {
-  var emitter = new EventEmitter();
+function MyCustomEmitter() {
+  this.name = 'my custom emitter';
+  util.inherits(MyCustomEmitter, EventEmitter);
+}
+
+MyCustomEmitter.prototype.readFile = function (files) {
+  let self = this;
+
   files.forEach(function (file) {
     fs.readFile(file, function (err, content) {
       if (err) {
-        /**
-         * The EventEmitter cannot just throw exceptions when an error condition
-         * occurs, as they would be lost in the event loop if the event is emitted
-         * asynchronously. Instead, the convention is to emit a special event, called
-         * error, and to pass an Error object as an argument.
-         */
-        return emitter.emit('error', err, file);
+        return self.emit('error', err, file);
       }
 
-      emitter.emit('fileread', file, content);
+      self.emit('fileread', file, content);
     });
   });
 
-  return emitter;
+  return self;
 }
 
-var e = readFile(['index.js', 'foo.js']);
-
-e.on('fileread', function (file, content) {
-  console.log('file read', file, content);
-});
-
-e.on('error', function (err, file) {
-  console.log('error', file, err);
-});
+const e = new MyCustomEmitter()
+e.readFile(['index.js', 'foo.js'])
+  .on('fileread', function (file, content) {
+    console.log('file read', file, content);
+    console.log();
+  }).on('error', function (err, file) {
+    console.log('error', file, err);
+    console.log();
+  });
