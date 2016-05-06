c65d8be2a01340c49c5ed4870db3ad206d91fffc
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-05-05 20:50:49 -0700

    Sample throwing error in then handler

--- promises.js
+++ promises.js
@@ -2,10 +2,14 @@
 'use strict';
 
 const p = new Promise(function (resolve, reject) {
-    reject(new Error('Something went wrong!'))
+    // reject(new Error('Something went wrong!'))
+    resolve('foo');
 });
 
-p.catch(function (err) {
-    console.log('Error: ' + err);
+p.then(function (result) {
+    console.log('got ', result);
+    throw new Error('Exception in rejection handler')
+}).catch(function (err) {
+    console.log(err);
 });
 
