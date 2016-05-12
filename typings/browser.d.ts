69dc349a458e0ac28bdebbfdf7065fb52eebb5f1
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-05-11 21:44:08 -0700

    Added chance library typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="browser/ambient/chance/index.d.ts" />
 /// <reference path="browser/ambient/glob/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/definitions/es6-promise/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="main/ambient/chance/index.d.ts" />
 /// <reference path="main/ambient/glob/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/definitions/es6-promise/index.d.ts" />
