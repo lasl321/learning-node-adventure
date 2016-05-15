701f046d93914668bb8e17e761e354e50daa5a46
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-05-14 21:56:40 -0700

    Added back pressure

--- entropy-server.js
+++ entropy-server.js
@@ -1,13 +1,24 @@
 var chance = require('chance').Chance();
+
 require('http').createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  while(chance.bool({likelihood: 95})) {
-    res.write(chance.string() + '\n');
+
+  function generateMore() {
+    while(chance.bool({likelihood: 95})) {
+      var shouldContinue = res.write(
+        chance.string({length: (16 * 1024) - 1})
+      );
+      if(!shouldContinue) {
+        console.log('Backpressure');
+        return res.once('drain', generateMore);
+      }
+    }
+    res.end('\nThe end...\n', function() {
+      console.log('All data was sent');
+    });
   }
-  res.end('\nThe end...\n');
-  res.on('finish', function() {
-    console.log('All data was sent'); 
-  });
+
+  generateMore();
 }).listen(8080, function () {
   console.log('Listening');
 });
